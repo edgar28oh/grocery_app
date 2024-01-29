346e0b241a90826860ddb68d6a88735038d2602e
Edgar Osorio-Hernandez <edgar28oh@gmail.com>
2024-01-29 03:10:12 -0800

    minor changes

--- backend/FredMeyer_API.py
+++ backend/FredMeyer_API.py
@@ -8,7 +8,6 @@ load_dotenv()
 # authentication
 auth_url = "https://api.kroger.com/v1/connect/oauth2/token"
 
-# Client credentials
 client_id = os.getenv("client_id")
 client_secret = os.getenv("client_secret")
 item = "tomato"
@@ -70,7 +69,7 @@ if response.status_code == 200:
                 # Make the API request
                 product_response = requests.get(product_details_url, headers=headers, params=params)
 
-                # Check if the request was successful (status code 200)
+                # Check if the request was successful
                 if product_response.status_code == 200:
                         product_data = product_response.json()
 
@@ -101,47 +100,10 @@ if response.status_code == 200:
                     if size['size'] == 'thumbnail':
                         thumbnail_url = size['url']
                         print("Thumbnail URL:", thumbnail_url)
-                        break  # Exit the loop once thumbnail URL is found
+                        break 
                 break
 
     else:
         print("Error:", search_response.status_code)
 else:
     print("Error:", response.status_code)
-
-
-"""
-                items_data = product_data["data"]
-                item_info = items_data["description"]
-                item_name = item_info + " " + item.get("size")
-
-                # Extract thumbnail URL
-                images = items_data.get("images", [])
-                thumbnail_url = None
-                for image in images:
-                    sizes = image.get("sizes", [])
-                    for size in sizes:
-                        if size["size"] == "thumbnail":
-                            thumbnail_url = size["url"]
-                            break
-                    if thumbnail_url:
-                        break
-
-                # Extract item details
-                items[item_name] = {
-                    "price": None,
-                    "promo_price": None,
-                    "img_url": thumbnail_url
-                }
-                items_list = items_data.get("items", [])
-                for item in items_list:
-                    price_info = item.get("price")
-                    if price_info:
-                        regular_price = price_info.get("regular")
-                        promo_price = price_info.get("promo")
-                        items[item_name]["price"] = regular_price
-                        items[item_name]["promo_price"] = promo_price
-                    else:
-                        print("Price information not available.")
-
-"""
