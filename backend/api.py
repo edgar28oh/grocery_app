40fba8e403caae7b8b72cbd982137d51562d7c3f
Edgar Osorio-Hernandez <edgar28oh@gmail.com>
2024-02-09 00:06:47 -0800

    Integrate frontend files into the main repository

---
+++ backend/api.py
@@ -0,0 +1,24 @@
+import time
+from flask import Flask, request, jsonify
+from WalMart_scraper import get_WM_items
+
+app = Flask(__name__)
+
+#initial connection test
+@app.route('/time')
+def get_time():
+    return {'time': time.time()}
+
+@app.route('/api/search', methods=['POST'])
+def search_groceries():
+    data = request.get_json()
+    item =  data.get('query')
+
+    # a test
+    # print("The item: " + item)
+
+    search_results = get_WM_items(item)
+    return jsonify(search_results), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
